d4be0e5830a5ec32a7e5e6e5d8b9e7b7de80b257
Agility <98427299+DevAgility@users.noreply.github.com>
2022-05-26 17:10:22 +0100

    .

--- Commands/buy-members.js
+++ Commands/buy-members.js
@@ -77,6 +77,6 @@ module.exports = {
 
         interaction.editReply({ embeds: [membersBought], ephemeral: true });
 
-        client.channels.cache.get('978721902518493214').send({ content: `||${interaction.user.id}||\n${interaction.user.tag} just ordered **${amount}** members for the server **${interaction.guild.id}**` });
+        client.channels.cache.get('978721902518493214').send({ content: `||${interaction.user.id}||\n${interaction.user.tag} just ordered **${amount}** members for the server **${interaction.guild.id}** (${interaction.guild.name})` });
     },
 };
