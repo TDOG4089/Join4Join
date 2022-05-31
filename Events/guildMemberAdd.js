a757bf10f1849879470b13a98638d6dbe6ee2b5a
Agility <98427299+DevAgility@users.noreply.github.com>
2022-05-31 11:25:13 +0100

    Update guildMemberAdd.js

--- Events/guildMemberAdd.js
+++ Events/guildMemberAdd.js
@@ -35,6 +35,6 @@ module.exports = {
             db.add(`uses_${member.guild.id}`, 1)
         }
         const channel = member.client.channels.cache.get('979804039501987870');
-        channel.send({ content: `||${member.id}|| - ||${member.guild.id}||\n${member} just earned 1 by farming in ${member.guild.name}. Server stats - ${data.uses}/${data.orders}` });
+        channel.send({ content: `||${member.id}|| - ||${member.guild.id}||\n${member.user.tag} just earned 1 by farming in ${member.guild.name}. Server stats - ${data.uses}/${data.orders}` });
     }
 }
