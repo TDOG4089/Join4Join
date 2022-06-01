2fe7e08636b50ada3f6abd3d3e8562f84fd79a52
Agility <98427299+DevAgility@users.noreply.github.com>
2022-06-01 15:27:30 +0100

    .

--- Commands/buy-members.js
+++ Commands/buy-members.js
@@ -15,11 +15,11 @@ module.exports = {
             .setRequired(true))
         .addStringOption((option) => option
             .setName('description')
-            .setDescription('Quick description of your server!')
+            .setDescription('Quick description of your bot!')
             .setRequired(true)),
     async execute(interaction, client) {
 
-        await interaction.deferReply();
+        await interaction.deferReply({ ephemeral: true });
 
         data = await get(interaction, interaction.user)
 
@@ -27,7 +27,7 @@ module.exports = {
         const description = interaction.options.getString('description');
 
         if (!interaction.member.permissions.has('ADMINISTRATOR'))
-            return interaction.editReply({ content: 'You don\'t have permissions to use this command!' });
+            return interaction.editReply({ content: 'You don\'t have permissions to use this command!', ephemeral: true });
 
         const minimumAmount = new MessageEmbed()
             .setDescription(`You need to spend/input at least **5** coins to buy members!\n(Maybe you do not have 5 coins)`)
@@ -35,22 +35,22 @@ module.exports = {
             .setTimestamp()
 
         if (amount < 5)
-            return interaction.editReply({ embeds: [minimumAmount] });
+            return interaction.editReply({ embeds: [minimumAmount], ephemeral: true });
         if (data.coins < 5)
-            return interaction.editReply({ embeds: [minimumAmount] });
+            return interaction.editReply({ embeds: [minimumAmount], ephemeral: true });
         if (amount > data.coins)
-            return interaction.editReply({ embeds: [minimumAmount] });
+            return interaction.editReply({ embeds: [minimumAmount], ephemeral: true });
 
         let link = data.code;
 
         if (link == 0) {
-            link = await interaction.channel.createInvite({ maxAge: 0 });
+            link = await interaction.channel.createInvite({ maxAge: 604800 });
+
             link = link.code;
         }
 
-        await interaction.client.fetchInvite('https://discord.gg/' + link).catch(err => {
-            console.log(err);
-            link = interaction.channel.createInvite({ maxAge: 0 });
+        await client.fetchInvite('https://discord.gg/' + link).catch(async x => {
+            link = await interaction.channel.createInvite({ maxAge: 0 });
             link = link.code;
             console.log(link);
         });
@@ -65,9 +65,9 @@ module.exports = {
         const membersBought = new MessageEmbed()
             .setTitle(`Order successfully completed`)
             .setColor(config.color)
-            .setDescription(`<@${interaction.user.id}>, you have bought **${amount}** members for your server!\nYou can now check the status of your order for **${interaction.guild.name}** by using the **/info** command!`)
+            .setDescription(`<@${interaction.user.id}>, you have bought**${amount}** members for your server!\nYou can now check the status of your order for **${client.guilds.cache.get(interaction.guild.id).name}** by using the **/info** command!`)
 
-        interaction.editReply({ embeds: [membersBought] });
+        interaction.editReply({ embeds: [membersBought], ephemeral: true });
 
         client.channels.cache.get('978721902518493214').send({ content: `||${interaction.user.id}||\n${interaction.user.tag} just ordered **${amount}** members for the server **${interaction.guild.id}** (${interaction.guild.name})` });
     },
