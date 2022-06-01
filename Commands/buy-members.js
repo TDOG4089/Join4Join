6d5e4472fab2a1b1600d5f7d31f1c811e28c76fa
Agility <98427299+DevAgility@users.noreply.github.com>
2022-06-01 10:55:02 +0100

    checking

--- Commands/buy-members.js
+++ Commands/buy-members.js
@@ -55,10 +55,10 @@ module.exports = {
 
         //await new Promise(resolve => setTimeout(resolve, 100));
 
-        db.set(`code_${interaction.guild.id}`, link)
-        db.set(`description_${interaction.guild.id}`, `${description === undefined ? "" : description}\nhttps://discord.gg/${link}`)
-        db.add(`orders_${interaction.guild.id}`, amount)
-        db.subtract(`coins_${interaction.user.id}`, amount)
+        //db.set(`code_${interaction.guild.id}`, link)
+        //db.set(`description_${interaction.guild.id}`, `${description === undefined ? "" : description}\nhttps://discord.gg/${link}`)
+       // db.add(`orders_${interaction.guild.id}`, amount)
+        //db.subtract(`coins_${interaction.user.id}`, amount)
 
         const membersBought = new MessageEmbed()
             .setTitle(`Order successfully completed`)
