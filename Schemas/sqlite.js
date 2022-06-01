caa20e704a9dc0b840188955b3ee3e8d29873e78
Agility <98427299+DevAgility@users.noreply.github.com>
2022-06-01 11:26:18 +0100

    Updated stuff

--- Schemas/sqlite.js
+++ Schemas/sqlite.js
@@ -29,7 +29,7 @@ const get = async (interaction, user) => {
     Object.entries(object_user_based).map(async x => {
         let variable = x[0]
         let value = x[1]
-        let item = await db.fetch(`${variable}_${user.id}`)
+        let item = await db.get(`${variable}_${user.id}`)
 
         if (item === null) data[variable] = value, db.set(`${variable}_${user.id}`, value)
         else data[variable] = item
@@ -38,14 +38,14 @@ const get = async (interaction, user) => {
     Object.entries(object_guild_based).map(async x => {
         let variable = x[0]
         let value = x[1]
-        let item = await db.fetch(`${variable}_${interaction.guild.id}`)
+        let item = await db.get(`${variable}_${interaction.guild.id}`)
         if (item === null) db.set(`${variable}_${interaction.guild.id}`, value), data[variable] = value
         else data[variable] = item
     })
     Object.entries(object_guild_user_based).map(async x => {
         let variable = x[0]
         let value = x[1]
-        let item = await db.fetch(`${variable}_${interaction.guild.id}_${user.id}`)
+        let item = await db.get(`${variable}_${interaction.guild.id}_${user.id}`)
         if (item === null) db.set(`${variable}_${interaction.guild.id}_${user.id}`, value), data[variable] = value
         else data[variable] = item
     })
