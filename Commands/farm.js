9d9426066c944814fe3999e56015c828764dc0a9
Agility <98427299+DevAgility@users.noreply.github.com>
2022-06-02 19:46:43 +0100

    .

--- Commands/farm.js
+++ Commands/farm.js
@@ -10,48 +10,20 @@ module.exports = {
         .setDescription('Join servers for coins!'),
     async execute(interaction, client) {
 
-        await interaction.deferReply({ ephemeral: true });
+        await interaction.deferReply();
 
         let orders = await db.all(`orders_`, { sort: ".data" });
 
-        let length = 5
-
-        orders = orders.filter(x => x.data > 0 && client.guilds.cache.get(x.ID.split("_")[1]) && client.guilds.cache.get(x.ID.split("_")[1]).members.cache.get(interaction.user.id) === undefined);
-
-        for (let i = 0; i < orders.length; i++) {
-
-            let handler = true
-
-            if (length > 5) {
-                return;
-            } else {
-
-                let id = orders[i].ID.split("_")[1]
-
-                let guild = client.guilds.cache.get(orders[i].ID.split("_")[1]).name
-
                 let code = await db.get(`code_${id}`)
-
-                await client.fetchInvite("https://discord.gg/" + code).then(link => {
-                    console.log(link.code)
-                    if (link.code === null) handler = false
-                }).catch(error => {
-                    handler = false
-                })
-
-                await new Promise(resolve => setTimeout(resolve, 0))
-
+                console.log('eee')
                 if (handler) {
                     let description = await db.get(`description_${id}`)
 
                     const invites = new MessageEmbed()
                         .setTitle('Invite link!')
-                        .setDescription(`Join this server for a coin!\n\n**${guild}** - ${description}`)
-                    interaction.editReply({ embeds: [invites], ephemeral: true })
+                        .setDescription(`Join this server for a coin!\n\ndiscord.gg/**${code}** - ${description}`)
+                    interaction.editReply({ embeds: [invites] })
                     length++
                 }
-            }
-        }
-        console.log('what')
     },
 };
