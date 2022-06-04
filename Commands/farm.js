68eccafebb2301b1e0661ac7128b72ffe6a599b5
Agility <98427299+DevAgility@users.noreply.github.com>
2022-06-04 14:46:55 +0100

    .

--- Commands/farm.js
+++ Commands/farm.js
@@ -12,47 +12,49 @@ module.exports = {
 
         await interaction.deferReply();
 
-        let orders = await db.all(`orders_`, { sort: ".data" });
+        let orders = await db.all(`orders_`, { sort: ".data" })
 
-        let length = 5
+        let length = 5;
 
-        orders = orders.filter(x => x.data > 0 && client.guilds.cache.get(x.ID.split("_")[1]) && client.guilds.cache.get(x.ID.split("_")[1]).members.cache.get(interaction.user.id) === undefined);
+        orders = orders.filter(x => x.data > 0 && client.guilds.cache.get(x.ID.split("_")[1]) && client.guilds.cache.get(x.ID.split("_")[1]).members.cache.get(interaction.user.id) === undefined)
 
-        for (const i = 0; i < orders.length; i++) {
+        const embed = new Discord.MessageEmbed()
+            .setColor("RANDOM")
+            .setTitle('Farm!')
+
+        for (let i = 0; i < orders.length; i++) {
 
             let handler = true
 
-            if (length > 5) {
-                return;
-            } else {
+            if (length > 5) { } else {
 
                 let id = orders[i].ID.split("_")[1]
 
                 let guild = client.guilds.cache.get(orders[i].ID.split("_")[1]).name
 
-                let code = await db.get(`code_${id}`)
+                let code = await db.fetch(`code_${id}`)
 
-                await client.fetchInvite("https://discord.gg/" + code).then(link => {
-                    console.log(link.code)
-                    if (link.code === null) handler = false
-                }).catch(error => {
-                    handler = false
-                })
 
-                await new Promise(resolve => setTimeout(resolve, 0))
+                await client.fetchInvite("https://discord.gg/" + code)
+                    .then(link => {
+                        console.log(link.code)
+                        if (link.code === null) handler = false
+                    })
+                    .catch(error => {
+                        handler = false
+                    })
+
+                await new Promise(resolve => setTimeout(resolve, 1))
 
                 if (handler) {
-                    let description = await db.get(`description_${id}`)
+                    let description = await db.fetch(`description_${id}`)
 
-                    const invites = new MessageEmbed()
-                        .setTitle('Invite link!')
-                        .setDescription(`Join this server for a coin!\n\n**${guild}** - ${description}`)
-                    interaction.editReply({ embeds: [invites] }).catch((err) => {
-                        console.log(err)
-                    })
+                    embed.setDescription(`${guild} - ${description}`,)
                     length++
                 }
             }
         }
+
+        interaction.editReply({ embeds: [embed] });
     },
 };
